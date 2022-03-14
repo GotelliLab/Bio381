80528082aad1babbbfad52d78049b12a895d0618
Beasley015 <beasley.em@gmail.com>
2022-03-14 15:13:35 -0400

    started HW8 tutorial

---
+++ Homeworks/Tutorial08_datasims_S2022.R
@@ -0,0 +1,15 @@
+##################################
+# Bio 381, HW 8: Simulating Data #
+# 14 Mar 2022                    #
+# E.M. Beasley                   #
+##################################
+
+# Why sim data?
+
+# Part 1: Normally distributed data ---------------------
+
+
+# Part 2: Abundance/count data ----------------------
+
+
+# Part 3: Occupancy/presence-absence data ---------------------
