d04f7998b63c45841a22c987e63233fde3ccd117
George <georgeni2442@gmail.com>
2025-04-06 21:25:29 -0400

    editing ggplot tidytuesday HW

--- Homeworks/ggplot_TidyTuesday_HW.R
+++ Homeworks/ggplot_TidyTuesday_HW.R
@@ -1,21 +1,19 @@
 
-#Find dataset that applies to either all of the plots, or find multiple examples-what kind of data works best for each? 
-#Give a brief description of the types of plots we're running, and what kind of dataset works best for them 
-
-
 #Loading in a TidyDataset from this link: https://github.com/rfordatascience/tidytuesday/blob/main/data/2021/2021-09-14/readme.md
 library(tidyverse)
 library(lubridate)
 library(ggbeeswarm)
 library(ggridges)
-library(treemap)
 
-tuesdata <- tidytuesdayR::tt_load('2021-09-14')
-billboard<- tuesdata$billboard
 
+tuesdata <- tidytuesdayR::tt_load('2021-09-14') #Use tidytuesdayR::tt_load() to find the correct weeks dataset that you want to use
+
+#This dataset has two csv's that we're going to use-Top 100 Billboard Chart songs from 1965-2017, and song features/characteristics of each one
+billboard<- tuesdata$billboard 
 song_features<-tuesdata$audio_features
 
-#So there are two datasets-one for the actual billboard list from 1965-2017, with weekly data, and then one for the audio features, with descriptions and characteristics of each song that can be joined/tracked. So immediately, one of the first things I notice is that we should probably join the song features to each song-this could lengthen the dataset quite a bit, so maybe we hold off on this for a second. 
+#So there are two datasets-one for the actual billboard list from 1965-2017, with weekly data, and then one for the audio features, with descriptions and characteristics of each song that can be joined/tracked. So immediately, one of the first things I notice is that we should probably join the song features to each song
+
 
 #Beeswarm Plot-typically used to present individual data points without overlap. Used when you want to show the overall distribution of a variable and the individual data points.
 #Ex. Plot of "Danceability/Energy/Speechiness", maybe the color is separated by decades?
@@ -56,14 +54,13 @@ density2d<-ggplot(data=decades,aes(danceability, tempo))+geom_bin_2d()+scale_fil
 density2d
 
 
-#Dendrogram-Maybe group songs based on how many times they have been in the Top 100 charts? So maybe we look at the top 10 list of songs for each decade, and then compare those (should be 50 or so songs), by Danceability to see how they're clustered 
-
-
+#Dendrogram-To visualize this and still be readable, let's first filter songs by decades and get the top 3 songs from each decade. Then we'll cluster it by danceability
 
 #So first group by decades
-#Extract Top 10 songs from each decade
-#Join Danceability metric to each song 
-#Create dendrogram
+#Filter the dataset for peak_position < 3 
+#Go ahead and only remove columns (url,week_id, week_position, Date, previous_week_position, weeks_on_chart,Year, peak_position) from the dataset
+#Get the distinct songs from the dataset
+#Slice for just the first 10 songs 
 
 decades_dendro<-decades%>%
   group_by(Decade)%>%
@@ -72,19 +69,17 @@ decades_dendro<-decades%>%
   mutate(Instance=instance)%>%
   select(-c(url,week_id, week_position, Date, previous_week_position, weeks_on_chart,Year, peak_position))%>%
   distinct()%>%
-  group_by(Decade)
+  ungroup()%>%
+  slice(1:10)
 
-ex<-decades_dendro[1:20,]
-  
 
-distance_matrix <- dist(ex$danceability)  # Euclidean distance
-hc <- hclust(distance_matrix, method = "complete")
+
+distance_matrix <- dist(decades_dendro$danceability)  # Euclidean distance to calculate the distances between songs for danceability
+hc <- hclust(distance_matrix, method = "complete") #Clustering algorithm using distance_matrix to group the songs together
 
 dendro<-as.dendrogram(hc)
 
 plot(dendro, main = "Dendrogram of Top Songs by Danceability",
      xlab = "Songs", ylab = "Distance", sub = "Complete Linkage")
 
-
-#Pull 20 songs from the last 5-10 years
 
