9ac916d8321270f6028281537f19ec1219d2549b
ngotelli <ngotelli@uvm.edu>
2025-04-10 09:07:02 -0400

    update scratch file

--- scratch.R
+++ scratch.R
@@ -5,119 +5,25 @@
 library(ggplot2)
 library(ggthemes)
 library(patchwork)
-# p1 <- ggplot(data= <DATA>) +
-#   aes(<MAPPINGS>) +
-#   <GEOM_FUNCTION>(aes(<MAPPINGS>),
-#                   stat=<STAT>,
-#                   position=<POSITION>) +
-#   <COORDINATE_FUNCTION> +
-#   <FACET_FUNCTION>
-
-# print(p1)
-
-# ggsave(plot=p1, 
-#        filename="MyPlot",
-#        width=5,
-#        height=3,
-#        units="in",
-#        device="pdf")
-
-d <- mpg # use built in mpg data frame
-str(d)
-table(d$fl)
-
-# basic histogram plot
-ggplot(data=d) +
-  aes(x=hwy) +
-  geom_histogram()
-
-ggplot(data=d) +
-  aes(x=hwy) +
-  geom_histogram(fill="khaki",color="black")
-
-# # basic density plot
-ggplot(data=d) +
-  aes(x=hwy) +
-  geom_density(fill="mintcream",color="blue")
-
-# basic scatter plot
-ggplot(data=d) +
-  aes(x=displ,y=hwy) +
-  geom_point() +
-geom_smooth() + 
-  geom_smooth(method="lm",col="red")
-
-
-# basic boxplot
-ggplot(data=d) +
-  aes(x=fl, y=cty) +
-  geom_boxplot()
-# basic boxplot
-ggplot(data=d) +
-  aes(x=fl, y=cty) +
-  geom_boxplot(fill="thistle")
-
-# basic barplot (long format)
-
-ggplot(data=d) +
-  aes(x=fl) +
-  geom_bar(fill="thistle",color="black")
-
-# bar plot with specified counts or meansw
-x_treatment <- c("Control","Low","High")
-y_response <- c(12,2.5,22.9)
-summary_data <- data.frame(x_treatment,y_response)
-
-ggplot(data=summary_data) +
-  aes(x=x_treatment,y=y_response) +
-  geom_col(fill=c("grey50","goldenrod","goldenrod"),col="black")
-
-# basic curves and functions
-my_vec <- seq(1,100,by=0.1)
-
-# plot simple mathematical functions
-d_frame <- data.frame(x=my_vec,y=sin(my_vec))
-ggplot(data=d_frame) +
-  aes(x=x,y=y) +
-  geom_line()
-
-
-# plot probability functions
-d_frame <- data.frame(x=my_vec,y=dgamma(my_vec,shape=5, scale=3))
-ggplot(data=d_frame) +
-  aes(x=x,y=y) +
-  geom_line()
-
-# plot user-defined functions
-my_fun <- function(x) sin(x) + 0.1*x
-d_frame <- data.frame(x=my_vec,y=my_fun(my_vec))
-ggplot(data=d_frame) +
-  aes(x=x,y=y) +
-  geom_line()
-
-p1 <- ggplot(data=d) +
-      (mapping=aes(x=displ,y=cty)) + 
-      geom_point()
-print(p1)
-
-p1 + theme_classic() # no grid lines
-p1 + theme_linedraw() # black frame
-p1 + theme_dark() # good for brightly colored points
-p1 + theme_base() # mimics base R
-p1 + theme_par() # matches current par settings in base
-p1 + theme_void() # shows data only
-p1 + theme_solarized() # good for web pages
-p1 + theme_economist() # many specialized themes
-p1 + theme_grey() # ggplots default theme
-
 library(extrafont)
 font_import()  # Imports all system fonts (run once)
 # loadfonts(device = "win")  # For Windows
 fonts()
 # Use the font in ggplot
-ggplot(mpg, aes(x = fl, y = cty,fill=fl)) +
+ggplot(data=d) +
+  aes(x = fl, y = cty,fill=fl)) +
   geom_boxplot() +
-   theme_linedraw(base_size=20,base_family="Noteworthy")
+   theme_classic(base_size=30, base_family="Geneva")
+
+ggplot(mtcars, aes(mpg, wt)) +
+  geom_point() +
+  labs(x="Fuel efficiency (mpg)", y="Weight (tons)",
+       title="Seminal ggplot2 scatterplot example",
+       subtitle="A plot that is only useful for demonstration purposes",
+       caption="Brought to you by the letter 'g'") +
+  theme_linedraw(base_size=20)
+
+
   # theme_linedraw(base_size=25,base_family = "serif")
 
 # built in themes
@@ -162,6 +68,7 @@ ggplot(mpg, aes(x = fl, y = cty,fill=fl)) +
 
 
 # hrbrthemes package
+# only ipsum and modern themes working
 # ggthemr package
 # ggtech package
 # ggdark package
